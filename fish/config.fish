7acd24089d591475f0ffbcd6d246dbc3fa258956
Kaleb Hornsby <kaleb@horns.by>
2014-08-23 14:40:59 +0000

    Remove ~/.lesshst from home directory

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,15 @@ if not set -q XDG_CONFIG_HOME
 end
 if not set -q XDG_CACHE_HOME
 	set -xg XDG_CACHE_HOME ~/.cache
+	mkdir -p $XDG_CACHE_HOME
 end
 if not set -q VIMINIT
 	set -xg VIMINIT 'let $MYVIMRC="$XDG_CONFIG_HOME/vim/xdg.vim" | source $MYVIMRC'
+end
+if not set -q LESSHISTFILE
+	# prevent less(1) from cluttering ~/ with ~/.lesshst file
+
+	set less_cache_dir $XDG_CACHE_HOME/less
+	mkdir -p $less_cache_dir
+	set -xg LESSHISTFILE $less_cache_dir/history
 end
