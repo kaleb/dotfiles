a3ce1b70607228f028673124046ece41e6091b3a
Kaleb Hornsby <kaleb@horns.by>
2016-07-13 09:14:07 -0400

    Add upstream fish functions
    One of my environments has an older version of fish that does not have
    these functions.

---
+++ fish/functions/export.fish
@@ -0,0 +1,16 @@
+# Added for backwards compaitbility with previous fishes
+function export --description 'Set global variable. Alias for set -gx, made for bash compatibility'
+	if test -z "$argv"
+		set
+		return 0
+	end
+	for arg in $argv
+		set -l v (string split -m 1 "=" -- $arg)
+		switch (count $v)
+		case 1
+			set -gx $v $$v
+		case 2
+			set -gx $v[1] $v[2]
+		end
+	end
+end
